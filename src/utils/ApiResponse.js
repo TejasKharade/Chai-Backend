241025564c5a31dbfc66b1f012a787bc22a7d2fc
TejasKharade <kharadetejas930@gmail.com>
2026-07-15 23:22:17 +0530

    Utils Code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse{
+    constructor(statusCode, data, message = "Success"){
+        this.StatusCode=statusCode
+        this.data=datathis.message=message
+        this.success=statusCode < 400
+    }
+}
