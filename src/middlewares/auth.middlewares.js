1a7928b07abb1ab7462c6774c51538f10d8ffc5e
TejasKharade <kharadetejas930@gmail.com>
2026-07-23 22:51:29 +0530

    update user account

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -13,9 +13,9 @@ export const verifyJWT = asyncHandler( async(req, _, next)=> {
     
         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
     
-        console.log("Before findById");
+         
         const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
-        console.log("After findById");
+         
     
         if(!user){
             // TODO: discuss about frontend
